7605b2eb5c055cccbb3230d416db4e3555f0eabd
Gaetano Taormina <gaetanotao04@gmail.com>
2026-06-03 00:46:49 +0200

    feat: add baggage fee logic based on weight to script.js

--- es_2/assets/js/script.js
+++ es_2/assets/js/script.js
@@ -1,8 +1,12 @@
 ﻿console.log("JS collegato correttamente");
 
-let Prezzo_Biglietto = 20 ;
-let Peso_Valigia = prompt ("Inserire il peso della propria valigia") ;
+let Prezzo_Biglietto = 36 ;
+let Peso_Valigia = prompt ("Inserire il peso della propria valigia in kg") ;
 
 Peso_Valigia = Number (Peso_Valigia) ;
 
-if (Peso_Valigia > 23) {}
+if (Peso_Valigia > 23) {
+    Prezzo_Biglietto = Prezzo_Biglietto + 20 ;
+}
+
+console.log(`il prezzo del biglietto è ${Prezzo_Biglietto}`) ;
