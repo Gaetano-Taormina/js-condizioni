66f1bbae81f93b98d36b5ef16b8f7d1cc1675fa3
Gaetano Taormina <gaetanotao04@gmail.com>
2026-06-03 01:30:24 +0200

    feat: add conditional logic for user age and height validation checks

--- es_4/assets/js/script.js
+++ es_4/assets/js/script.js
@@ -1,14 +1,14 @@
 ﻿console.log("JS collegato correttamente");
 
-let eta = prompt("dichiarare l'età dell'utente: ") ;
+l("dichiarare l'età dell'utente: ") ;
 eta = Number(eta) ;
 
-if (eta<14) {
+if (eta < 14) {
     alert("Accesso Negato: l'utente è troppo giovane") ;
 } else {
     let h = prompt("dichiarare l'altezza dell'utente(cm): ") ;
     h = Number(h) ;
-    if (h<140){
+    if (h < 140){
         alert("Accesso Negato: l'utente non rispetta i requisiti") ;
     } else {
         console.log("Buon divertimento");
