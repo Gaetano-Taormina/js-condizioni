e94ce3d52e9173444fcf72d65d3392b8de24132d
Gaetano Taormina <gaetanotao04@gmail.com>
2026-06-03 22:59:14 +0200

    feat: implement discount logic for loyalty card holders in exercise 6

--- es_6/assets/js/script.js
+++ es_6/assets/js/script.js
@@ -1,1 +1,19 @@
 ﻿console.log("JS collegato correttamente");
+
+let tessera = prompt ("Hai la tessera? (si/no)") ;
+
+tessera = tessera.trim().toLowerCase() ;
+
+if (tessera === "si") {
+    tessera = 1 ;
+} else {
+    tessera = 0 ;
+}
+
+let prezzo = prompt("Inserire il prezzo del prodotto: ") ;
+
+if (tessera) {
+    prezzo = prezzo * 0.9 ;
+}
+
+console.log("Il prezzo da pagare è: ", prezzo)
