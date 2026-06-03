eab8685265a6edbfb6f623be33c3dfff16dc9654
Gaetano Taormina <gaetanotao04@gmail.com>
2026-06-03 22:31:36 +0200

    feat: implement conditional age-based greeting logic

--- es_5/assets/js/script.js
+++ es_5/assets/js/script.js
@@ -1,1 +1,12 @@
 ﻿console.log("JS collegato correttamente");
+
+let eta = prompt("inserire l'età dell'utente: ") ;
+eta  = Number(eta) ;
+
+if (eta < 18) {
+    console.log("Ciao") ;
+} else if (eta >= 60) {
+    console.log("Salve") ;
+} else {
+    console.log("Benvenuto") ;
+} 
